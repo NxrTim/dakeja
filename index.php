45b5e968ef0eaaaff50eca8d715264dfd71bbe0a
NxrTim <timmmyboy@gmx.de>
2022-02-09 23:51:39 +0100

    Update

--- index.php
+++ index.php
@@ -6,42 +6,44 @@ session_start();
 $url = "dakeja.fleischer-home.de";
 ?>
 <link rel="stylesheet" href="https://<?= $url; ?>/style.css" type="text/css">
-<div id="main">
-<?php
-if(isset($_POST['dakeja_number'])){
-    $_SESSION['dakeja_number'] = htmlspecialchars($_POST['dakeja_number']);
-    header('location: https://'.$url.'/');
-    exit();
-}
+<div id="wrapper">
+    <div id="main">
+    <?php
+    if(isset($_POST['dakeja_number'])){
+        $_SESSION['dakeja_number'] = htmlspecialchars($_POST['dakeja_number']);
+        header('location: https://'.$url.'/');
+        exit();
+    }
 
-if(!isset($_SESSION['dakeja_number'])){
-?>
-        <form method="post">
-            <p id="topic-1">Deine Dakeja-Nummer oder Tikok-Name:</p>
-            <input type="text" name="dakeja_number" id="dakeja_number" placeholder="15" autofocus>
-            <input type="submit" id="button-1" value="loslegen">
-        </form>
-<?php
-}else{
+    if(!isset($_SESSION['dakeja_number'])){
     ?>
-    <p id="topic-2">Deine Dakeja-Nummer/ TikTokName: <b><?= $_SESSION['dakeja_number'] ?></b></p><br>
-    <head>
-        <script type="text/javascript">
-            var updateDiv = function ()
-            {
-                $('#target').load('reload.php?num=<?= $_SESSION['dakeja_number'] ?>');
-            }
+            <form method="post">
+                <p id="topic-1">Deine Dakeja-Nummer oder Tikok-Name:</p>
+                <input type="text" name="dakeja_number" id="dakeja_number" placeholder="15" autofocus>
+                <input type="submit" id="button-1" value="loslegen">
+            </form>
+    <?php
+    }else{
+        ?>
+        <p id="topic-2">Deine Dakeja-Nummer/ TikTokName: <b><?= $_SESSION['dakeja_number'] ?></b></p><br>
+        <head>
+            <script type="text/javascript">
+                var updateDiv = function ()
+                {
+                    $('#target').load('reload.php?num=<?= $_SESSION['dakeja_number'] ?>');
+                }
 
-            var deinTimer = window.setInterval(updateDiv, 5000);
-        </script>
-    </head>
-    <body>
+                var deinTimer = window.setInterval(updateDiv, 5000);
+            </script>
+        </head>
+        <body>
 
 
-    <div id="target">
-        <p id="please_wait">Bitte warten der Inhalt wird geladen...</p>
+        <div id="target">
+            <p id="please_wait">Bitte warten der Inhalt wird geladen...</p>
+        </div>
+        </body>
     </div>
-    </body>
 </div>
 <?php
 }
