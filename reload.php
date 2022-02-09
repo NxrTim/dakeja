af12a022075a696d69a72e24ee8bf283921fede3
NxrTim <timmmyboy@gmx.de>
2022-02-09 16:19:03 +0100

    Update

--- reload.php
+++ reload.php
@@ -27,7 +27,7 @@ if($settings_live['value'] == "false"){
 }else{
     ?>
     <script>
-        var countDownDate = new Date("Feb 9, 2022 16:19:25").getTime();
+        var countDownDate = new Date("Feb 9, 2022 16:29:25").getTime();
         var x = setInterval(function() {
             var now = new Date().getTime();
             var distance = countDownDate - now;
