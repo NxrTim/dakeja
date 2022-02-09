19ad334f7b618668699594ee0cee5e838f033790
NxrTim <timmmyboy@gmx.de>
2022-02-09 15:34:32 +0100

    Update

--- reload.php
+++ reload.php
@@ -3,11 +3,11 @@ error_reporting(E_ALL | E_STRICT);
 ini_set("display_errors", 1);
 date_default_timezone_set('Europe/Berlin');
 
-$liveversteigerung = false;
+$liveversteigerung = "false";
 ?>
 <p id="time">Letzte Aktualisierung: <?php echo date('H:i:s'); ?></p>
 <?php
-if($liveversteigerung === true){
+if($liveversteigerung == "false"){
     ?>
 <p id="topic-3">Aktuell läuft keine Artiekl Versteigerung.</p>
 
