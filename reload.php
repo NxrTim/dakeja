047a55130e2ab8c42556e9df333c057335c73e93
NxrTim <timmmyboy@gmx.de>
2022-02-09 15:33:45 +0100

    Update

--- reload.php
+++ reload.php
@@ -2,6 +2,8 @@
 error_reporting(E_ALL | E_STRICT);
 ini_set("display_errors", 1);
 date_default_timezone_set('Europe/Berlin');
+
+$liveversteigerung = false;
 ?>
 <p id="time">Letzte Aktualisierung: <?php echo date('H:i:s'); ?></p>
 <?php
