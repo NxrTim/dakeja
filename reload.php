00c65f16ff20bdbb3b4cff3473090b91b7913de9
NxrTim <timmmyboy@gmx.de>
2022-02-09 16:12:26 +0100

    Update

--- reload.php
+++ reload.php
@@ -13,6 +13,8 @@ $con1 = new mysqli($db_host, $db_user, $db_password, $db_name);
 $sql = "SELECT * FROM `settings` WHERE setting='live'";
 $settings_live = $pdo1->query($sql)->fetch();
 
+$sql = "SELECT * FROM `settings` WHERE setting='start'";
+$settings_start = $pdo1->query($sql)->fetch();
 
 ?>
 <p id="time">Letzte Aktualisierung: <?php echo date('H:i:s'); ?></p>
@@ -24,7 +26,25 @@ if($settings_live['value'] == "false"){
 <?php
 }else{
     ?>
-<p><b>00:59</b></p>
+    <script>
+        var countDownDate = "<?php echo $settings_start; ?>";
+        var x = setInterval(function() {
+            var now = new Date().getTime();
+            var distance = countDownDate - now;
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // Display the result in the element with id="demo"
+            document.getElementById("countdown").innerHTML = minutes + ":" + seconds;
+
+            // If the count down is finished, write some text
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("countdown").innerHTML = "EXPIRED";
+            }
+        }, 1000);
+    </script>
+    <b><p id="countdown">00:59</p></b>
     <p id="topic-5">Verteigerung aktiv!</p>
     <p>Nehme jetzt teil und ersteige den Artikel!</p>
 <?php
