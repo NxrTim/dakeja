7191472cdf1f056b8ee43582b5c9198085cbcf27
NxrTim <timmmyboy@gmx.de>
2022-02-09 15:27:04 +0100

    Update

--- reload.php
+++ reload.php
@@ -3,7 +3,4 @@ error_reporting(E_ALL | E_STRICT);
 ini_set("display_errors", 1);
 date_default_timezone_set('Europe/Berlin');
 ?>
-<script>
-    console.log( 'Loaded Background');
-</script>
-<?php echo date('H:i:s'); ?>
+<p id="time">Letzte Aktualisierung: <?php echo date('H:i:s'); ?></p>
