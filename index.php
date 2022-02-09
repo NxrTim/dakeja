c42e69140318e3880a185e5959df8a1d864253be
NxrTim <timmmyboy@gmx.de>
2022-02-09 14:16:25 +0100

    Update

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <title>Dakeja &bull; Versteigerung</title>
 <p>Deine Dakeja-Nummer oder Tikok-Name:</p>
-<input type="text" name="dakeja_number">
+<input type="text" name="dakeja_number" placeholder="15">
+<input type="submit" value="loslegen">
 <?php
