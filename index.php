a031bdc4b5ca77e958e58cf7f54f07826fbf6b1a
NxrTim <timmmyboy@gmx.de>
2022-02-09 14:20:39 +0100

    Update

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <title>Dakeja &bull; Versteigerung</title>
-<p>Deine Dakeja-Nummer oder Tikok-Name:</p>
-<input type="text" name="dakeja_number" placeholder="15">
-<input type="submit" value="loslegen">
 <?php
+$url = "dakeja.fleischer-home.de";
+if(isset($_POST['dakeja_number'])){
+    $_SESSION['dakeja_number'] = htmlspecialchars($_POST['dakeja_number']);
+    header('location: https://'.$url.'/');
+    exit();
+}
+if(!isset($_SESSION['dakeja_number'])){
+?>
+        <form method="post">
+            <p>Deine Dakeja-Nummer oder Tikok-Name:</p>
+            <input type="text" name="dakeja_number" placeholder="15">
+            <input type="submit" value="loslegen">
+        </form>
+<?php
+}else{
+    ?>
+<p>Deine Dakeja-Nummer/ TikTokName: <b><?= $_SESSION['dakeja_number'] ?></b></p>
+<?php
+}
