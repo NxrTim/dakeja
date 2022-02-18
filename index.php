bbba7119db12d0ab6c0b8aaa91b97eed2445704a
NxrTim <timmmyboy@gmx.de>
2022-02-18 22:05:45 +0100

    Update

--- index.php
+++ index.php
@@ -58,12 +58,24 @@ $url = "dakeja.fleischer-home.de";
                 .then((willDelete) => {
                     if (!willDelete) {
                         swal("Leider ist für dich die Teilnahme nicht möglich.");
-                        <?php
-                        session_destroy();
-                        session_start();
-                        header('location: https://'.$url.'/');
-                        exit();
-                        ?>
+                        var cookieList = (document.cookie) ? document.cookie.split(';') : [];
+
+                        var cookieValues = {};
+                        for (var i = 0, n = cookieList.length; i != n; ++i) {
+                            var cookie = cookieList[i];
+                            var f = cookie.indexOf('=');
+                            if (f >= 0) {
+                                var cookieName = cookie.substring(0, f);
+                                var cookieValue = cookie.substring(f + 1);
+
+                                console.log ("cookieName + " + cookieName + " cookieValue " + cookieValue);
+
+                                if (!cookieValues.hasOwnProperty(cookieName)) {
+                                    cookieValues[cookieName] = cookieValue;
+                                }
+                            }
+                        }
+                        document.location="https://<?= $url ?>.de";
                     }
                 });
         </script>
