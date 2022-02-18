4e93a2e0710c520854bedf70a3566007225a4cff
NxrTim <timmmyboy@gmx.de>
2022-02-18 22:31:18 +0100

    Update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <title>Dakeja &bull; Versteigerung</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<meta name="viewport" content="width=device-width, initial-scale=0.7, user-scalable=no">
 <?php
 session_start();
 $url = "dakeja.fleischer-home.de";
